39f7987bbee7f742dd82b6a01bda27d7a2cd0e20
Everth <everth386@hotmail.com>
2023-06-26 10:46:31 -0500

    se inicia la creacion del proyecto para la parte back de la app

---
+++ src/test/java/com/com/theprodigy/prodigicount/MsProdigiCountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.com.theprodigy.prodigicount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProdigiCountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
